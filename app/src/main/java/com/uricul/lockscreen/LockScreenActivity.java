ff8e1f6b1a1dc2d3897e1176bc7f91a1f5ae32ec
정주호 <uricul@daumcorp.com>
2014-08-25 18:55:06 +0900

    LockScreen 상태바 투명하게

--- app/src/main/java/com/uricul/lockscreen/LockScreenActivity.java
+++ app/src/main/java/com/uricul/lockscreen/LockScreenActivity.java
@@ -3,9 +3,13 @@ package com.uricul.lockscreen;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import java.lang.reflect.Field;
 
 
 public class LockScreenActivity extends Activity {
@@ -48,7 +52,54 @@ public class LockScreenActivity extends Activity {
                 }
             }
         });
+
+        ApplyTransparentStatusBar();
     }
 
+    int ResolveTransparentStatusBarFlag() {
+        String[] libs = getPackageManager().getSystemSharedLibraryNames();
+        String reflect = null;
+
+        if( libs == null )
+            return 0;
+
+        for( String lib : libs ) {
+            if( lib.equals("touchwiz") ) {
+                Log.d("LockScreenActivity", "ResolveTransparentStatusBarFlag : reflect:" + lib);
+                reflect = "SYSTEM_UI_FLAG_TRANSPARENT_BACKGROUND";
+            }
+            else if( lib.startsWith("com.sonyericsson.navigationbar") ) {
+                Log.d("LockScreenActivity", "ResolveTransparentStatusBarFlag : reflect:" + lib);
+                reflect = "SYSTEM_UI_FLAG_TRANSPARENT";
+            }
+        }
+
+        if (reflect == null)
+            return 0;
+
+        try {
+            Field field = View.class.getField(reflect);
+            if (field.getType() == Integer.TYPE) {
+                int result = field.getInt(null);
+                Log.d("LockScreenActivity", "ResolveTransparentStatusBarFlag : " + result);
+                return result;
+            }
+        } catch( Exception e )
+        {
+            Log.d("LockScreenActivity", "ResolveTransparentStatusBarFlag Exception:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    void ApplyTransparentStatusBar() {
+        Window window = getWindow();
+        if( window != null) {
+            View decor = window.getDecorView();
+            if( decor != null)
+                decor.setSystemUiVisibility(ResolveTransparentStatusBarFlag());
+        }
+    }
 }
 
